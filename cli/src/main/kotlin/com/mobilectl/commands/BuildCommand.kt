89065e4b048deac6ce34fc1593e1ce0218af4952
Ahmed <Ahmed.nader1994@gmail.com>
2025-11-05 12:54:09 +0200

    fix linking

--- cli/src/main/kotlin/com/mobilectl/commands/BuildCommand.kt
+++ cli/src/main/kotlin/com/mobilectl/commands/BuildCommand.kt
@@ -5,6 +5,7 @@ import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.optional
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
+import com.mobilectl.commands.buildCommand.BuildHandler
 import kotlinx.coroutines.runBlocking
 
 class BuildCommand : CliktCommand(name = "build") {
